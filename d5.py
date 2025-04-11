dc4c13320ec0756b19a55722f6bb9e6b2e89875b
RobloxGOAT2137 <w.jasinski@zs2wagrowiec.pl>
2025-04-11 16:04:45 +0200

    Update d5.py

--- d5.py
+++ d5.py
@@ -1,10 +1,2 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return jsonify(message="Hello, world!")
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+name = input("Jak masz na imię? ")
+print(f"Siema, {name}!")
